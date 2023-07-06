7ec8a47fa1733d02e8c83aa84ca2ddd744f4c46a
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-07-07 00:12:36 +0530

    added job details page connectivity

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -14,7 +14,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
           resizeMode='contain'
-          style={styles.logImage}
+          style={styles.logoImage}
         />
       </TouchableOpacity>
 
