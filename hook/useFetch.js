6457137758d615d43be93cd7e1c4094442a5e9af
greeenboi <suvan.gowrishanker.204@gmail.com>
2023-07-06 15:42:26 +0530

    more changes and .env file

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -10,7 +11,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Key": '58764ebd65msh0c5633663967ecap1fbf9djsn4fa96b5875d7' ,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
